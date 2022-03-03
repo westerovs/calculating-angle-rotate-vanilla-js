7a5f0127dcfbdea5eee6f9e88f6cb6cfbbc1dff4
Александр <westerovs@mail.ru>
2022-03-03 19:20:34 +0300

    create class

--- script.js
+++ script.js
@@ -1,56 +1,62 @@
-const wheel = document.querySelector('.wheel')
-const dot = document.querySelector('.wheel__dot')
-wheel.style.transform = 'rotate(0deg)'
-
-let progress = 0
-let startProgress = 0
-let val = 0
-let nex = null
-let degreeAngle = null
-let startTouches = null
-
-// ------------------------------------- TOUCHSTART
-const touchStart = (event) => {
-    startProgress = progress
+class Wheel {
+    constructor() {
+        this.wheel = document.querySelector('.wheel')
     
-    nex = wheel.style.transform    // nex  -  текущий градус поворота при touchstart
-    nex = parseFloat(nex.slice(7)) // уже не помню почему 7, что-то вычислял
+        this.progress = 0
+        this.startProgress = 0
+        this.val = 0
+        this.nex = null
+        this.degreeAngle = null
+        this.startTouches = null
+        
+        this.init()
+    }
     
-    // получаем первые координаты касания
-    startTouches = {
-        x: event.changedTouches[0].pageX,
-        y: event.changedTouches[0].pageY
+    init = () => {
+        this.wheel.style.transform = `rotate(${ this.startProgress }deg)`
+    
+        this.wheel.addEventListener('touchstart', this.touchStart)
+        this.wheel.addEventListener('touchmove', this.touchMove)
+    }
+    
+    touchStart = (event) => {
+        this.startProgress = this.progress
+        
+        this.nex = this.wheel.style.transform    // this.nex  -  текущий градус поворота при touchstart
+        this.nex = parseFloat(this.nex.slice(7)) // уже не помню почему 7, что-то вычислял
+        
+        // получаем первые координаты касания
+        this.startTouches = {
+            x: event.changedTouches[0].pageX,
+            y: event.changedTouches[0].pageY
+        }
+    }
+    
+    touchMove = (event) => {
+        // получаем координаты касания
+        const touch = {
+            x: event.changedTouches[0].pageX,
+            y: event.changedTouches[0].pageY
+        }
+        
+        // центр круга
+        const center = {
+            x: this.wheel.offsetLeft + this.wheel.offsetWidth / 2,
+            y: this.wheel.offsetTop + this.wheel.offsetHeight / 2
+        }
+        
+        // дистанция от - до +
+        let distance = Math.atan2(
+          (center.x - touch.x) * (center.y - this.startTouches.y) - (center.y - touch.y) * (center.x - this.startTouches.x),
+          (center.x - touch.x) * (center.x - this.startTouches.x) + (center.y - touch.y) * (center.y - this.startTouches.y)
+        )
+        
+        distance *= -1
+        
+        this.degreeAngle = distance * (180 / Math.PI)
+        this.val = this.degreeAngle + this.nex
+        this.wheel.style.transform = `rotate(${ this.val }deg)`
     }
 }
 
-// ------------------------------------- TOUCHMOVE
-const touchMove = (event) => {
-    // получаем координаты касания
-    const touch = {
-        x: event.changedTouches[0].pageX,
-        y: event.changedTouches[0].pageY
-    }
-    
-    // центр круга
-    const center = {
-        x: wheel.offsetLeft + wheel.offsetWidth / 2,
-        y: wheel.offsetTop + wheel.offsetHeight / 2
-    }
-    
-    // дистанция от - до +
-    let distance = Math.atan2(
-      (center.x - touch.x) * (center.y - startTouches.y) - (center.y - touch.y) * (center.x - startTouches.x),
-      (center.x - touch.x) * (center.x - startTouches.x) + (center.y - touch.y) * (center.y - startTouches.y)
-    )
-    
-    distance *= -1
-    
-    degreeAngle = distance * (180 / Math.PI)
-    val = degreeAngle + nex
-    wheel.style.transform = `rotate(${ val }deg)`
-}
-
-wheel.addEventListener('touchstart', touchStart)
-wheel.addEventListener('touchmove', touchMove)
-
-
+new Wheel()
